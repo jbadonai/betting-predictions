c261b76de050d47b5724d41850cff286fedb1e49
Ajeyemi Alajede <ajeyemi.alajede@9mobile.com.ng>
2024-05-12 10:41:02 +0100

    update

--- updateDataWithCurrentResult.py
+++ updateDataWithCurrentResult.py
@@ -7,6 +7,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 import pandas as pd
 import time
+from datetime import datetime
+
+
+def get_today():
+    # Get today's date
+    today = datetime.today()
+
+    # Extract the day component
+    day = today.day
+
+    return day
+
 
 main_url = 'https://www.sportybet.com/'
 data_url = 'https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240420045904ord28819061'
@@ -223,78 +235,18 @@ def extract_data(driver):
         print(f"An error occurred in Extract_Data(): {e}")
         pass
 
-    #
-    # for index, url in enumerate(urls):
-    #     print(f"Loading and extracting data page {index}...")
-    #     try:
-    #         print('loading data url...')
-    #         # print(data_url)
-    #         driver.get(url)
-    #     except TimeoutException:
-    #         pass
-    #
-    #     # print('done loading data url....')
-    #     WebDriverWait(driver, 60).until(
-    #                 EC.presence_of_element_located((By.CSS_SELECTOR, data_table_class))
-    #             )
-    #     time.sleep(2)
-    #     print('Extracting data')
-    #     # get the table
-    #     table = driver.find_element(By.CSS_SELECTOR, data_table_class)
-    #
-    #     # get all rows
-    #     rows = table.find_elements(By.CSS_SELECTOR, row_data_class)
-    #     # print(f">>>total row data: {len(rows)}")
-    #
-    #     # loop through the rows
-    #
-    #     for row in rows:
-    #         # homeTeam = row.find_element(By.CSS_SELECTOR,home_team_class).text
-    #         # awayTeam = row.find_element(By.CSS_SELECTOR, away_team_class).text
-    #         try:
-    #             homeScore = row.find_element(By.CSS_SELECTOR, home_score_class).text
-    #             awayScore = row.find_element(By.CSS_SELECTOR, away_score_class).text
-    #         except:
-    #             homeScore = "-"
-    #             awayScore = '-'
-    #
-    #         teams = row.find_elements(By.CLASS_NAME, team_label_class)
-    #         homeTeam = teams[0].text
-    #         awayTeam = teams[1].text
-    #
-    #         result = None
-    #         try:
-    #             if int(homeScore) > int(awayScore):
-    #                 result = "H"
-    #             elif int(homeScore) < int(awayScore):
-    #                 result = "A"
-    #             elif int(homeScore) == int(awayScore):
-    #                 result = "D"
-    #         except:
-    #             result = None
-    #             continue
-    #
-    #         data.append((homeTeam, awayTeam, homeScore, awayScore, result))
-    #
 
-    # for d in data:
-    #     print(d)
-    #
-    # input('ms')
-
-def get_urls_with_data(driver):
-
-    url_list = []
+def get_urls_with_data(driver, currentDate, start_page=1):
+    print(F"[DEBUG] using date:{currentDate} and starting page of: {start_page}")
     football_data = []
     basketball_data = []
 
-    current_date = 11
-
-    for page_no in range(100):
-        page_url = f"https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets?page={page_no + 1}"
+    current_date = int(currentDate)
+    cap = 0
+    for page_no in range(start_page, 100):
+        page_url = f"https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets?page={page_no}"
         date_css = "span.m-order-left"
         bar_css = "div.m-order-bar"
-        back_button_css = "div.back-btn"
 
         # print(f'loading page {page_url}')
         time.sleep(1)
@@ -303,8 +255,6 @@ def get_urls_with_data(driver):
         except TimeoutException:
             pass
 
-
-        # print(f"Total bars: {len(all_bars)}")
         for x in range(5):
             print(f"\rWorking on [{x+1}/5]: {page_url}", end="")
 
@@ -330,7 +280,6 @@ def get_urls_with_data(driver):
             # extract day out of the date
             today = str(dt).split("/")[0]
             time.sleep(3)
-            # thisUrl = driver.current_url # this might not be necessary again sincce i will extract here
 
             # Data Extraction on current page
             print(f"\rWorking on [{x + 1}/5][Extracting Data]: {page_url}", end="")
@@ -339,14 +288,15 @@ def get_urls_with_data(driver):
             football_data.extend(fb_data)
             basketball_data.extend(bb_data)
 
-
-
             if str(today).strip() != str(current_date).strip():
-                print()
-                print(f"CURRENT DATE SPECIFIED COMPLETED!")
-                return football_data, basketball_data
-
-            # url_list.append(thisUrl)
+                cap += 1
+                if cap < 5:
+                    print(f"DATE OUT OF RANGE, TRYING NEXT...")
+                    continue
+                else:
+                    print()
+                    print(f"CURRENT DATE SPECIFIED COMPLETED!")
+                    return football_data, basketball_data
 
             driver.back()
             time.sleep(3)
@@ -356,19 +306,21 @@ def get_urls_with_data(driver):
     return football_data, basketball_data
 
 
+#-----------------------------------------------------------------
+# ENTRY
+#-----------------------------------------------------------------
+dt = input("Specify date(day): ")
+if dt == "":
+    dt = get_today()
+else:
+    dt = int(dt)
 
-# ans = input("Have you updated the url list for the last game? ")
-# s = input("Select Sport > [{F}ootball | {B}asketball]: ")
-# if s.lower() == 'f' or s.lower() == '':
-#     sport = "football"
-# elif s.lower() == 'b':
-#     sport = "basketball"
-# else:
-#     print("Unrecognized sport! Football selected for you!")
-#     sport = "football"
+startPage = input("Specify start page: ")
+if startPage == "":
+    startPage = 1
+else:
+    startPage = int(startPage)
 
-# if ans == 'y' or ans == 'Y' or ans == '':
-# load the web browser
 driver = webdriver.Chrome()
 
 # got to the web page
@@ -378,7 +330,7 @@ login_to_sportybet(driver, '8022224284', "Afolayemi1")
 print('done logging in')
 
 time.sleep(5)
-footballData, basketballData = get_urls_with_data(driver)
+footballData, basketballData = get_urls_with_data(driver, dt, startPage)
 print()
 
 if len(footballData) > 1:
@@ -388,49 +340,4 @@ if len(footballData) > 1:
 if len(basketballData) > 1:
     print("updating basketball data on file..")
     update_excel_data(basketballData, sport="basketball")
-
-
-#
-#     urls_string = """https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511132843ord06999052
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511132302ord06662034
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511100642ord93543907
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511094149ord92053176
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511092617ord91195614
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511091852ord90789365
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511091714ord90701642
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511091535ord90613563
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511090239ord89922067
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511090113ord89845528
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511081942ord87668797
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511081801ord87589831
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511075541ord86589923
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511054515ord82043666
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511053654ord81855833
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511053442ord81809573
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511045957ord81210257
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511045634ord81157403
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511045338ord81116403
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511045131ord81087789
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511044943ord81063971
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511042525ord80774021
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511040951ord80618259
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511040624ord80587773
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511040434ord80571240
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511040239ord80554671
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511040005ord80532809
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511035340ord80475480
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511035209ord80462667
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511034922ord80439214
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511034452ord80401825
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511034243ord80383489
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511033840ord80348890
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511033522ord80322231
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511032723ord80259020
-# https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240511031718ord80181887"""
-#
-#     urls = urls_string.split("\n")
-#     data = []
-
-
-
 
