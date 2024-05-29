451511f4bf6f74a8174dd1daae49fbdb447906a1
Ajeyemi Alajede <ajeyemi.alajede@9mobile.com.ng>
2024-05-29 05:41:20 +0100

    update

---
+++ filter.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import ast
+import re
+
+# code = "302"
+dates = input("Filter for which date? :")
+
+path1 = rf"C:\Users\ajeyemi.alajede\OneDrive - Emerging Markets Telecommunication Services Limited\Desktop\predictions\{dates}.txt"
+path = "fb_result.txt"
+extraData = []
+
+with open(path1, 'r', encoding='utf-8') as f:
+    data = f.read()
+
+
+def filter(code, data):
+    global extraData
+    dataList = data.split("\n")
+    begin = False
+
+    dataGroup = []
+
+    group = []
+    home = None
+    away = None
+    game_code = None
+    accuracy = None
+
+    sporter_counter = 0
+    for d in dataList:
+        if d.__contains__("Game Time"):
+            begin = True
+
+        if d.__contains__("*"):
+            begin = False
+            continue
+
+        if begin is True:
+            if d != "":
+                group.append(d)
+
+            # extract home and away team
+            if d.__contains__('Teams'):
+                home = d.split(":")[1].strip()
+                away = d.split(":")[2].strip()
+
+            if d.__contains__("{"):
+                sporter_counter += 1
+                if sporter_counter == 1:
+                    accuracy = d.split(" ")[-1].strip()
+                elif sporter_counter == 2:
+                    game_code = d.split(" ")[-1].strip()
+                    sporter_counter = 0
+
+
+        if begin is False:
+            if len(group) > 0:
+                # apply filter
+
+                if str(group).__contains__(f"[{code}]"):
+
+                    x = "\n".join(group)
+
+                    x += "\n\n\t\t\t\t***************************************************\n"
+                    dataGroup.append(x)
+
+            extraData.append((home, away, game_code, accuracy))
+            group.clear()
+            home = None
+            away = None
+            accuracy = None
+            game_code = None
+            sporter_counter = 0
+
+    return "\n".join(dataGroup)
+
+codeList = ['302','500','050','410','320','221','113','212','041','203','140','104','311','230','401']
+
+final_filtered_data = ""
+
+print(f"Filtered data for {dates}")
+final_filtered_data += f"Filtered data for {dates}\n"
+# print()
+for code in codeList:
+    fd = filter(code, data)
+    lb = "<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n"
+
+    final_filtered_data += f"{fd}\n{lb}"
+
+with open(f"Filtered/filtered_{dates}.txt", 'w', encoding='utf-8') as f:
+    f.write(final_filtered_data)
+
+
+with open(f"Filtered/extraData_{dates}.txt", 'w', encoding='utf-8') as f:
+    f.write(str(extraData))
+
+
+print('COMPLETED!')
+
+# to excell
+#-----------------------
+
+# Read data from the text file
+with open(f"Filtered/extraData_{dates}.txt", 'r', encoding='utf-8') as file:
+    data = file.read()
+
+# Remove leading and trailing square brackets if present
+data = data.strip()[1:-1]
+
+# Split the string into a list of tuples using regular expressions
+tuple_strings = re.findall(r'\(([^)]+)\)', data)
+
+# Convert the tuple strings into actual tuples
+parsed_data = []
+for tuple_str in tuple_strings:
+    # Split by comma but ignore commas inside quotes
+    items = re.split(r",\s*(?![^()]*\))", tuple_str)
+    # Strip quotes and spaces
+    items = [item.strip().strip("'") for item in items]
+    parsed_data.append(tuple(items))
+
+# Create a DataFrame with the specified headings
+df = pd.DataFrame(parsed_data, columns=['home', 'away', 'code', 'accuracy'])
+# Save the DataFrame to an Excel file
+df.to_excel(f"Filtered/extraData_{dates}.xlsx", index=False)
--- test.py
+++ test.py
@@ -1,358 +1,80 @@
-# # # testing result extractor
-# # from selenium import webdriver
-# # from selenium.webdriver.common.by import By
-# # from selenium.webdriver.support.ui import WebDriverWait
-# # from selenium.webdriver.support import expected_conditions as EC
-# # from selenium.webdriver.common.keys import Keys
-# # from selenium.common.exceptions import TimeoutException
-# # import pandas as pd
-# # import time
-# #
-# # main_url = 'https://www.sportybet.com/'
-# # data_url = 'https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240420045904ord28819061'
-# #
-# #
-# # def login_to_sportybet(driver, username, password):
-# #
-# #     # Navigate to the main URL
-# #     try:
-# #         driver.get('https://www.sportybet.com/')
-# #     except TimeoutException:
-# #         pass
-# #
-# #     try:
-# #         # Find the username field and enter the username
-# #         username_field = WebDriverWait(driver, 10).until(
-# #             EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="phone"]'))
-# #         )
-# #         username_field.send_keys(username)
-# #
-# #         # Find the password field and enter the password
-# #         password_field = WebDriverWait(driver, 10).until(
-# #             EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="psd"]'))
-# #         )
-# #         password_field.send_keys(password)
-# #
-# #         # Find the login button and click
-# #         login_button = WebDriverWait(driver, 10).until(
-# #             EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[name="logIn"]'))
-# #         )
-# #         login_button.click()
-# #         time.sleep(2)
-# #
-# #         # # Wait for the login process to complete
-# #         # WebDriverWait(driver, 10).until(
-# #         #     EC.url_changes(driver.current_url)
-# #         # )
-# #
-# #         # Optional: You can add more actions after successful login
-# #
-# #     finally:
-# #         # Close the browser session
-# #         pass
-# #
-# #
-# # def update_excel_data_old(new_data):
-# #     # Load the Excel file
-# #     excel_file = 'ml.xlsx'
-# #     df = pd.read_excel(excel_file)
-# #     # print(df)
-# #
-# #     # Unpack the new data tuple
-# #     home, away, home_score, away_score, status = new_data
-# #
-# #     # Check if the home and away teams exist in the DataFrame
-# #     match_index = df[(df['home'].str.strip() == home) & (df['away'].str.strip() == away)].index
-# #
-# #     if len(match_index) > 0:
-# #         match_index = match_index[0]  # Get the first matching index
-# #
-# #         # Update the status column
-# #         df.loc[match_index, 'status'] = status
-# #
-# #         # Add home_score and away_score columns if not already present
-# #         if 'home_score' not in df.columns:
-# #             df['home_score'] = ''
-# #         if 'away_score' not in df.columns:
-# #             df['away_score'] = ''
-# #
-# #         # Update home_score and away_score columns
-# #         df.loc[match_index, 'home_score'] = home_score
-# #         df.loc[match_index, 'away_score'] = away_score
-# #
-# #         # Write the updated DataFrame back to the Excel file
-# #         df.to_excel(excel_file, index=False)
-# #         print("Data updated successfully.")
-# #     else:
-# #         print("No matching data found.")
-# #
-# #
-# # def update_excel_data(new_data_list):
-# #     # Load the Excel file
-# #     excel_file = 'ml.xlsx'
-# #     df = pd.read_excel(excel_file)
-# #
-# #     for new_data in new_data_list:
-# #         print(f"working on {new_data}....")
-# #         # Unpack the new data tuple
-# #         home, away, home_score, away_score, status = new_data
-# #
-# #         # Check if the home and away teams exist in the DataFrame
-# #         match_index = df[(df['home'].str.strip() == home) & (df['away'].str.strip() == away)].index
-# #
-# #         if len(match_index) > 0:
-# #             match_index = match_index[0]  # Get the first matching index
-# #
-# #             # Update the status column
-# #             df.loc[match_index, 'status'] = status
-# #
-# #             # Add home_score and away_score columns if not already present
-# #             if 'home_score' not in df.columns:
-# #                 df['home_score'] = ''
-# #             if 'away_score' not in df.columns:
-# #                 df['away_score'] = ''
-# #
-# #             # Update home_score and away_score columns
-# #             df.loc[match_index, 'home_score'] = home_score
-# #             df.loc[match_index, 'away_score'] = away_score
-# #
-# #     # Convert 'home_score' and 'away_score' columns to numeric
-# #     df['home_score'] = pd.to_numeric(df['home_score'], errors='coerce')
-# #     df['away_score'] = pd.to_numeric(df['away_score'], errors='coerce')
-# #
-# #     # Remove rows with status 'pending'
-# #     df = df[df['status'] != 'pending']
-# #
-# #     # Write the updated DataFrame back to the Excel file
-# #     df.to_excel(excel_file, index=False)
-# #     print("Data updated successfully.")
-# #
-# # '''
-# # table: div.m-order-detail-wrap.list
-# # each row: div.selection-detail-wrap
-# # data: div.selection-body (this can replace each row as it's unique in each row)
-# # home: p.team-label home (.text)
-# # away: p.team-label (.text)
-# # home score = div.home-score main (.text)
-# # away score = div.away-score main (.text)
-# # '''
-# #
-# # data_table_class = 'div.m-order-detail-wrap.list'
-# # row_data_class = 'div.selection-body'
-# # home_team_class = 'p.team-label.home'
-# # away_team_class = 'p.team-label'
-# # team_label_class = 'team-label'
-# # home_score_class = 'div.home-score.main'
-# # away_score_class = 'div.away-score.main'
-# #
-# # # load the web browser
-# # driver = webdriver.Chrome()
-# #
-# # # got to the web page
-# # driver.set_page_load_timeout(30)
-# # print('trying to login...')
-# # login_to_sportybet(driver, '8022224284', "Afolayemi1")
-# # print('done logging in')
-# # time.sleep(5)
-# #
-# # urls = [
-# #     "https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240420045904ord28819061",
-# #     "https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240420051900ord29081319",
-# #     "https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240420134051ord52427015",
-# #     "https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240420141346ord54122793",
-# #     "https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240420143842ord55118646",
-# #     "https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240420152714ord57391458",
-# #     "https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240420192347ord66572238",
-# #     "https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240420192524ord66610824",
-# #     "https://www.sportybet.com/ng/my_accounts/bet_history/sport_bets/detail/240420195420ord67294812"
-# # ]
-# # data = []
-# # for index, url in enumerate(urls):
-# #     print(f"Loading and extracting data page {index}...")
-# #     try:
-# #         print('loading data url...')
-# #         # print(data_url)
-# #         driver.get(url)
-# #     except TimeoutException:
-# #         pass
-# #
-# #     # print('done loading data url....')
-# #     WebDriverWait(driver, 60).until(
-# #                 EC.presence_of_element_located((By.CSS_SELECTOR, data_table_class))
-# #             )
-# #     time.sleep(2)
-# #     print('Extracting data')
-# #     # get the table
-# #     table = driver.find_element(By.CSS_SELECTOR, data_table_class)
-# #
-# #     # get all rows
-# #     rows = table.find_elements(By.CSS_SELECTOR, row_data_class)
-# #     # print(f">>>total row data: {len(rows)}")
-# #
-# #     # loop through the rows
-# #
-# #     for row in rows:
-# #         # homeTeam = row.find_element(By.CSS_SELECTOR,home_team_class).text
-# #         # awayTeam = row.find_element(By.CSS_SELECTOR, away_team_class).text
-# #         try:
-# #             homeScore = row.find_element(By.CSS_SELECTOR, home_score_class).text
-# #             awayScore = row.find_element(By.CSS_SELECTOR, away_score_class).text
-# #         except:
-# #             homeScore = "-"
-# #             awayScore = '-'
-# #
-# #         teams = row.find_elements(By.CLASS_NAME, team_label_class)
-# #         homeTeam = teams[0].text
-# #         awayTeam = teams[1].text
-# #
-# #         result = None
-# #         try:
-# #             if int(homeScore) > int(awayScore):
-# #                 result = "H"
-# #             elif int(homeScore) < int(awayScore):
-# #                 result = "A"
-# #             elif int(homeScore) == int(awayScore):
-# #                 result = "D"
-# #         except:
-# #             result = None
-# #             continue
-# #
-# #         data.append((homeTeam, awayTeam, homeScore, awayScore, result))
-# #
-# #
-# # # for d in data:
-# # #     print(d)
-# #
-# # update_excel_data(data)
-#
-#
-# import Levenshtein
-#
-# def are_similar(string1, string2, threshold=0.3):
-#     """
-#     Check if two strings are similar based on Levenshtein distance.
-#
-#     Args:
-#     string1 (str): First string.
-#     string2 (str): Second string.
-#     threshold (float): Similarity threshold. Default is 0.8.
-#
-#     Returns:
-#     bool: True if the strings are similar, False otherwise.
-#     """
-#     distance = Levenshtein.distance(string1.lower(), string2.lower())
-#
-#     max_length = max(len(string1), len(string2))
-#     similarity = 1 - (distance / max_length)
-#     return similarity >= threshold
-#
-# # Test cases
-# sample_data = [
-#     ("Stade Lausanne Ouchy", "Stade Ls Ouchy"),
-#     ("Grasshopper Club Zurich", "Grasshoppers"),
-#     ("OFI Crete", "OFI"),
-#     ("Volos NPS", "Volos"),
-#     ("Kuwait", "Al-Kuwait"),
-#     ("FC Vaduz", "Vaduz"),
-#     ("FC Wil 1900", "FC Wil"),
-#     ('FC Vaduz', 	'Baden'),
-#     ('FC Metalist 1925 Kharkiv' ,	'Metalist 1925 U19')
-# ]
-#
-# # for string1, string2 in sample_data:
-# #     print(f"{string1} : {string2} -> Similar: {are_similar(string1, string2)}")
-#
-#
-#
-# def are_the_same(a, b):
-#
-#     small = None
-#
-#     if len(a) < len(b):
-#         small = a
-#         big = b
-#     else:
-#         small = b
-#         big = a
-#
-#     big = big.replace("-", " ")
-#     small = small.replace("-", " ")
-#
-#     small_list = str(small).split(" ")
-#     big_list = str(big).split(" ")
-#
-#     trueCounter = 0
-#     falseCounter = 0
-#     for tester in small_list:
-#         if tester in big_list:
-#             trueCounter += 1
-#         else:
-#             found = False
-#             if len(tester) > 2:
-#                 for b in big_list:
-#                     if tester[:-1] == b or b[:-1] == tester:
-#                         trueCounter += 1
-#                         found = True
-#                         break
-#
-#                 if found is True:
-#                     break
-#                 else:
-#                     falseCounter += 1
-#
-#     same = trueCounter > falseCounter
-#     return same
-#
-#
-#
-# for data in sample_data:
-#     a = data[0]
-#     b = data[1]
-#     print(f"{a}, {b} | same: {are_the_same(a,b)}")
+data = """
+     Game Time:      [ 00:00 ]
+                                   Teams:          CSD San Martin  :  Argentino de Quilmes
+                                   Prediction:     ['H', 'H', 'H', 'H', 'D']
+                                   Accuracy:       [45.75, 36.6, 37.91, 44.44, 41.83]
+                                   Summary:        {'H': 41, 'D': 42}  [83]
+                                                   {'H': 4, 'D': 1} [401]
+                                                   H/D  [Home or Draw [1X]]
+                                   ********************************************************************************
+                                   
+                                   Game Time:      [ 00:15 ]
+                                   Teams:          Trem AP  :  Rio Branco FC AC
+                                   Prediction:     ['H', 'H', 'H', 'H', 'H']
+                                   Accuracy:       [45.75, 39.22, 39.87, 44.44, 41.83]
+                                   Summary:        {'H': 42}  [42]
+                                                   {'H': 5} [500]
+                                                   H/D  [Home or Draw [1X]]
+                                   ********************************************************************************
+                                   
+                                   Game Time:      [ 01:00 ]
+                                   Teams:          Atletico Tucuman  :  CA Platense
+                                   Prediction:     ['H', 'D', 'D', 'H', 'D']
+                                   Accuracy:       [45.75, 36.6, 40.52, 44.44, 41.83]
+                                   Summary:        {'H': 45, 'D': 40}  [85]
+                                                   {'H': 2, 'D': 3} [203]
+                                                   H/D  [Home or Draw [1X]]
+                                   ********************************************************************************
+                                   
+                                   Game Time:      [ 01:00 ]
+                                   Teams:          Botafogo FC SP  :  Gremio Novorizontino SP
+                                   Prediction:     ['D', 'D', 'H', 'H', 'D']
+                                   Accuracy:       [45.75, 38.56, 37.91, 44.44, 41.83]
+                                   Summary:        {'H': 41, 'D': 42}  [83]
+                                                   {'D': 3, 'H': 2} [203]
+                                                   H/D  [Home or Draw [1X]]
+                                                   
+                                        *******************************************************************************
+"""
+
+path = r"C:\Users\ajeyemi.alajede\OneDrive - Emerging Markets Telecommunication Services Limited\Desktop\predictions\27.txt"
 
 
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+path = "fb_result.txt"
+with open(path, 'r', encoding='utf-8') as f:
+    data = f.read()
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+def filter(code, data):
+    dataList = data.split("\n")
+    begin = False
 
-        self.setWindowTitle("Embedded Chrome Browser")
-        self.setGeometry(100, 100, 800, 600)
+    dataGroup = []
+    group = []
+    for d in dataList:
+        if d.__contains__("Game Time"):
+            begin = True
 
-        layout = QVBoxLayout()
-        self.browser_widget = QWidget()
-        self.browser_widget.setLayout(layout)
+        if d.__contains__("*"):
+            begin = False
+            continue
 
-        self.webview = QWebEngineView()
-        layout.addWidget(self.webview)
+        if begin is True:
+            if d != "":
+                group.append(d)
 
-        self.setCentralWidget(self.browser_widget)
+        if begin is False:
+            if len(group) > 0:
+                # apply filter
 
-        self.init_browser()
+                if str(group).__contains__(f"[{code}]"):
 
-    def init_browser(self):
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-        driver = webdriver.Chrome(options=chrome_options)
+                    x = "\n".join(group)
 
-        driver.get("https://www.example.com")  # Load a website
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))  # Wait for page to load
-        html = driver.page_source
-        self.webview.setHtml(html)
+                    x += "\n\n\t\t\t\t\t\t\t\t\t***************************************************\n"
+                    dataGroup.append(x)
+            group.clear()
 
-        driver.quit()
+    return "\n".join(dataGroup)
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+
+print(filter('500', data))
